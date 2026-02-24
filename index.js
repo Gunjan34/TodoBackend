5c4270c9994b6929dbd9e67a5313d054b2df31e2
Gunjan43 <gunjanmahara63@gmail.com>
2026-02-24 14:45:26 +0530

    fixed port handling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/auth");
 const todoRoutes = require("./routes/TodoRoute");
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
