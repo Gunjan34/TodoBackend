e9096c89ceb6ed4a4f70f2814b37d477035eee0f
Gunjan43 <gunjanmahara63@gmail.com>
2026-02-25 10:39:31 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 const authRoutes = require("./routes/auth");
 const todoRoutes = require("./routes/TodoRoute");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+
+/* Middleware */
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/todos",todoRoutes);
+app.use(cors({
+    origin: "*",   // Change to frontend IP later for security
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-//DataBase Connection
+/* Routes */
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todoRoutes);
+
+/* Database Connection */
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"));
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-
-
-app.listen(PORT,()=>{
-    console.log("Server is running on " + process.env.PORT)
-})
+/* Server Start */
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
